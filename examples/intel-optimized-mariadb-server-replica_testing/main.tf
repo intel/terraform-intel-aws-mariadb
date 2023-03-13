d2e8ae714d2aa41e22f9a850b311c47dcfe6eb97
kevinbleckmann <123095645+kevinbleckmann@users.noreply.github.com>
2023-03-13 13:22:00 -0700

    fixed replica destroy
    found issue with replica not being destroyed.

--- examples/intel-optimized-mariadb-server-replica_testing/main.tf
+++ examples/intel-optimized-mariadb-server-replica_testing/main.tf
@@ -8,6 +8,7 @@ module "optimized-mariadb-server" {
   rds_identifier = "mariadb-dev"
   db_password    = var.db_password
 
+
   # Update the vpc_id below for the VPC that this module will use. Find the default vpc-id in your AWS account
   # from the AWS console or using CLI commands. In your AWS account, the vpc-id is represented as "vpc-",
   # followed by a set of alphanumeric characters. One sample representation of a vpc-id is vpc-0a6734z932p20c2m4
@@ -23,4 +24,6 @@ module "optimized-mariadb-server-read-replica" {
   vpc_id                 = "vpc-5ea60f23"
   db_replicate_source_db = module.optimized-mariadb-server.db_instance_id
   kms_key_id             = module.optimized-mariadb-server.db_kms_key_id
+  create_subnet_group    = false
+  skip_final_snapshot    = true
 }
