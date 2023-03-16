7d9c55a286fc2991edf7d6865f6a349da088b5ca
kevinbleckmann <123095645+kevinbleckmann@users.noreply.github.com>
2023-03-16 14:03:11 -0700

    Initial AWS Mariadb Module release
    * Mariadb intial module 1 test pass
    
    completed 1 passing test after modifying sql module
    
    * Developed Example 2
    
    Updated config and variables for 2nd example mariadb deployment
    
    * Read Replica Example
    
    Updated Read Replica Example and Tested build successfully
    
    * removed tf-docs file since it's a duplicate
    
    * added readme to examples
    
    * Updating Branch for collaboration with Kinder
    
    * updated final settings
    
    Maria DB
    
    * Update main.tf
    
    * Update main.tf
    
    * fixed replica destroy
    
    found issue with replica not being destroyed.
    
    * Update main.tf
    
    removed vpc id set to standard <YOUR-VPC-ID-HERE>
    
    * fixed image url's
    
    updated readme file image locations
    
    * terraform-docs: automated action
    
    ---------
    
    Co-authored-by: Kinder Wischmeier <kinder.wischmeier@intel.com>
    Co-authored-by: Lucas Melo <lucas.melo@intel.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- examples/intel-optimized-mariadb-server-replica_testing/main.tf
+++ examples/intel-optimized-mariadb-server-replica_testing/main.tf
@@ -7,10 +7,21 @@ module "optimized-mariadb-server" {
   source         = "intel/aws-mariadb/intel"
   rds_identifier = "mariadb-dev"
   db_password    = var.db_password
-
   # Update the vpc_id below for the VPC that this module will use. Find the default vpc-id in your AWS account
   # from the AWS console or using CLI commands. In your AWS account, the vpc-id is represented as "vpc-",
   # followed by a set of alphanumeric characters. One sample representation of a vpc-id is vpc-0a6734z932p20c2m4
-  vpc_id = "YOUR-VPC-ID-HERE"
+  vpc_id = "<YOUR-VPC-ID-HERE>"
 }
 
+module "optimized-mariadb-server-read-replica" {
+  source         = "intel/aws-mariadb/intel"
+  rds_identifier = "mariadb-dev-replica"
+  db_password    = var.db_password
+
+  # Update the vpc-id below. Use the same vpc-id as the one used in the prior module.
+  vpc_id                 = "<YOUR-VPC-ID-HERE>"
+  db_replicate_source_db = module.optimized-mariadb-server.db_instance_id
+  kms_key_id             = module.optimized-mariadb-server.db_kms_key_id
+  create_subnet_group    = false
+  skip_final_snapshot    = true
+}
